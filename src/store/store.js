327c06e07f96e1446dd986ad5b7c123e1ee57468
bilalmuneer877 <bilalmuneer877@gmail.com>
2024-12-03 18:49:03 +0500

    first commit

---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import homeslice from "./homeSlice";
+
+export const store = configureStore({
+    reducer: {
+        home: homeslice,
+    },
+})
